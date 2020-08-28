65417bcb93876967abf689a77b57fe355448b63b
Daniel Destro <daniel.carmo@luizalabs.com>
2020-08-28 13:05:27 -0300

    Fixed Cucumber Tests runtime with Maven.

--- src/test/java/cucumber/salad/AppIT.java
+++ src/test/java/cucumber/salad/AppIT.java
@@ -1,15 +1,11 @@
 package cucumber.salad;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import cucumber.salad.App;
 
 @SpringBootTest
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @ContextConfiguration(classes = App.class)
 public class AppIT {
 
